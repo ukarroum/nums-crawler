5a152d9304b044201d05f16e56f1cbf6b06c34bf
Yassir Karroum <ukarroum17@gmail.com>
2016-08-17 14:44:31 +0100

    Mise à jout du README (ajout de plus de détails). Le code tor a été remplacé par un import torPythonInt

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Numbers Crawler
 
 Un script qui récupére les numéros de téléphone et cordonnées depuis des sites d'annonces.
-Pour le moment les sites sont: Souk.ma.
 
 Le script devra permettre prochainement de récupérer aussi les cordonnées reatives à ces numéros.
-Il utilise la bibliothéque BeautifulSoup de python.
+Il utilise la bibliothéque [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) de python et [torPythonInt](https://github.com/ukarroum/tor-python-interface).
+
+# Dependances
+
+``` 
+# apt-get install python3-bs4
+# pip3 install stem
+# apt-get install python3-pycurl
+# apt-get install tor
+# pip3 install torPythonInt
+```
+
+
+# Support
+
+Site | Support
+---- | -------------
+Souk.ma | Récupération des numéros.
--- nums_crawler.py
+++ nums_crawler.py
@@ -7,24 +7,15 @@
  * Un simple script qui permet de récupérer des numéros de téléphone marocains en masse depuis des sites d'annonce en ligne.
  */
 """
+from torPythonInt import *
 from bs4 import BeautifulSoup
-import stem.process
 from stem.util import term
-import pycurl
 import io
 import sys
 
 OKGREEN = '\033[92m'
 BOLD = '\033[1m'
 ENDC = '\033[0m'
-
-SOCKS_PORT = 7000
-
-tor_process = 0
-use_tor = False   # Si tor est utilisé le parametre devient true
-
-use_contry_code = True
-country_code = "fr"  # Un choix arbitraire qui est du à un nombre plutot elevé des relais francais.
 
 
 def souk_ma_crawler(begin=1, n=10):
@@ -99,86 +90,10 @@ def save_file(links, file):
             continue
         f.write(link + '\n')
 
-""" ============== Tor ============== """
-
-""" Ces 3 fonctions ont été "grandement" inspirés de : https://stem.torproject.org/tutorials/to_russia_with_love.html"""
-
-
-def init_tor():
-
-    global tor_process
-
-    if use_contry_code:
-        tor_process = stem.process.launch_tor_with_config(
-            config={
-                'SocksPort': str(SOCKS_PORT),
-                'ExitNodes': '{'+country_code+'}'
-            }
-        )
-    else:
-        tor_process = stem.process.launch_tor_with_config(
-            config={
-                'SocksPort': str(SOCKS_PORT)
-            }
-        )
-
-    global use_tor
-    use_tor = True
-
-
-def kill_tor():
-
-    tor_process.kill()
-
-
-def get_html(url):
-    """
-    Uses pycurl to fetch a site using the proxy on the SOCKS_PORT.
-    """
-
-    output = io.BytesIO()
-
-    query = pycurl.Curl()
-    query.setopt(pycurl.URL, url)
-
-    if use_tor:
-        query.setopt(pycurl.PROXY, 'localhost')
-        query.setopt(pycurl.PROXYPORT, SOCKS_PORT)
-        query.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5_HOSTNAME)
-
-    query.setopt(pycurl.USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0')
-    query.setopt(pycurl.WRITEFUNCTION, output.write)
-
-    try:
-        query.perform()
-        return output.getvalue()
-    except pycurl.error as exc:
-        return "Unable to reach %s (%s)" % (url, exc)
-
-
-def new_identity():
-    if use_tor:
-        print(term.format("Nouvelle identite : "+get_ip()+"\n", term.Color.BLUE))
-        kill_tor()
-        init_tor()
-    else:
-        print(term.format("Lancement de Tor.\n", term.Attr.BOLD))
-        init_tor()
-
-
-def get_ip():
-
-    soup = BeautifulSoup(get_html("http://whatismyipaddress.com/"), "html.parser")
-
-    return soup.find(id="section_left").find_all('div')[1].a.string
-
 
 if __name__ == "__main__":
 
     nums = set(souk_ma_crawler(1, 500))
 
-    # for num in nums:
-    # print(num)
-
-    save_file(nums, "out2")
+    save_file(nums, "out")
 
